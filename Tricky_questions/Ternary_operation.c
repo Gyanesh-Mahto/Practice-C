91fd04ad1b57f5252a127371fa8f7c5ca50c6150
Gyanesh Mahto <61919561+Gyanesh-Mahto@users.noreply.github.com>
2020-08-26 20:09:46 +0530

    Update Ternary_operation.c

--- Tricky_questions/Ternary_operation.c
+++ Tricky_questions/Ternary_operation.c
@@ -16,10 +16,18 @@ printf("%d\n%d\n", b, c);
 printf("%d\n%d\n", b, c);
 (c>10?b:c)=20;  //Code to repeat 20 only once in ternary operation. But only compiles with g++ and not gcc
 printf("b=%d\nc=%d\n", b, c);
+*((c>10)?&b:&c)=20; //3. C-code to repeat 20 only once in ternary operation.
+printf("b=%d\nc=%d\n", b, c);
 }
 /*
+2.
 This is due to the precedence issue. In C (gcc): conditional operator(?:) has higher precedence than 
 Assignment operator(=),
 But in C++(g++): conditional operator(?:) has same priority than Assignment operator(=). So, it compile 
 the program with g++
+
+3.
+Since b and c are lvalue and so ((c>10)?&b:&c) is considered as lvalue and 20 is considered rvalue which is not correct. So, in order to
+assign value of 20, need to assigb value to reference, So, b and c are referenced and then whole conditional operator is de-refernced.
+So, it will work in C.
 */
